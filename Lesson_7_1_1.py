85b97b910c0e162d0b3d15e6cac466631171eb76
Антон <antbrdulin@gmail.com>
2022-10-18 14:50:46 +0300

    Lesson_7_1_1_done!

--- Lesson_7_1_1.py
+++ Lesson_7_1_1.py
@@ -1,4 +1,10 @@
+def first_control():
+    return secret > guess
+def second_control():
+    return secret < guess
+
 import random
+
 secret = int(input("Введите число от 1 до 77: "))
 
 first_item = 1
@@ -10,10 +16,10 @@ if secret == guess:
     print('С первого раза!')
 else:
     while guess != secret:
-        if secret > guess:
+        if first_control():
             first_item = guess
             guess = random.randint(first_item, second_item)
-        elif secret < guess:
+        elif second_control():
             second_item = guess
             guess = random.randint(first_item, second_item)
 
